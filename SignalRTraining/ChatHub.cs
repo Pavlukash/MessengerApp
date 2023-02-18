23c3fa2244b6c3f93f1a175e2232dfb3730a7557
Pavel Tarasov <gomucyl@gmail.com>
2023-02-18 10:27:25 +0100

    Initial commit. Simple chat was created

---
+++ SignalRTraining/ChatHub.cs
@@ -0,0 +1,42 @@
+using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRTraining;
+
+public class ChatHub : Hub
+{
+    public async Task Send(string message)
+    {
+        await Clients.All.SendAsync("Receive", message, Context.ConnectionId);
+    }
+    
+    public override async Task OnConnectedAsync()
+    {
+        await Clients.All.SendAsync("Notify", $"{Context.ConnectionId} has joined chat");
+        
+        var context = Context.GetHttpContext();
+
+        if (context is not null)
+        {
+            if (context.Request.Cookies.ContainsKey("name"))
+            {
+                if (context.Request.Cookies.TryGetValue("name", out var userName))
+                {
+                    Console.WriteLine($"name = {userName}");
+                }
+            }
+
+            Console.WriteLine($"UserAgent = {context.Request.Headers["User-Agent"]}");
+
+            Console.WriteLine($"RemoteIpAddress = {context.Connection.RemoteIpAddress}");
+        }
+
+        await base.OnConnectedAsync();
+    }
+    
+    public override async Task OnDisconnectedAsync(Exception? exception)
+    {
+        await Clients.All.SendAsync("Notify", $"{Context.ConnectionId} has disconnected");
+        
+        await base.OnDisconnectedAsync(exception);
+    }
+}
